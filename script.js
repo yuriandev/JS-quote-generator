c2a8378e56e9e4718e981be8e39f3a79eae4291b
Yuri An <yuriandev@gmail.com>
2023-03-01 23:41:50 +0600

    feat: add loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   // Check if author is blank and replace it with 'Unknown'
@@ -25,10 +39,12 @@ function newQuote() {
   }
 
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from api
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
